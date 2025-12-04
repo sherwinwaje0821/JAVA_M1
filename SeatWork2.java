2602a1b6d7f704bb18017c73e59a0880721e854a
Sherwin P. Waje <sherwin.p.waje@live.com>
2025-12-04 16:20:50 +0800

    M2_Activity2

---
+++ SeatWork2.java
@@ -0,0 +1,11 @@
+package ph.com.bpi.hello.M2_Act2;
+
+
+public class SeatWork2 {
+    public static void main(String[] args) {
+
+        WhiteBoard wb = new WhiteBoard();
+        int sum = wb.addNumbers(5,3);
+        System.out.println("The total is: " + String.valueOf(sum) );
+    }
+}
---
+++ WhiteBoard.java
@@ -0,0 +1,11 @@
+package ph.com.bpi.hello.M2_Act2;
+
+
+public class WhiteBoard {
+
+     int addNumbers(int addend1, int addend2){
+        int sum = addend1 + addend2;
+        return sum;
+    }
+}
+
