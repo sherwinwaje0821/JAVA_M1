1fdca8449e09d14affdc2136d7573cefe8c594f1
Sherwin P. Waje <sherwin.p.waje@live.com>
2026-03-21 14:55:14 +0800

    M9 Exercise 2

--- M9_Ex2/src/main/java/com/example/demo/DemoApplication.java
+++ M9_Ex2/src/main/java/com/example/demo/DemoApplication.java
@@ -9,22 +9,6 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         
-        /*
-         * 
-         * get
-http://localhost:9091/api/v1/movies/all
-get
-http://localhost:9091/api/v1/movies/1
-post
-http://localhost:9091/api/v1/movies
-{
-  "title": "Titanic",
-  "director": "James Cameron",
-  "year": 1997,
-  "genre": "Romance"
-}
-delete
-http://localhost:9091/api/v1/movies/6
-         */
+       
     }
 }
